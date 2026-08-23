b4057587e2439d35cd78a6b0a592e6499a12d1c0
Layog, Joshus <idkwutishappen@gmail.com>
2026-08-23 19:28:15 +0800

    "Just a minor backbutton adjustment"

--- frontend/src/components/BackButton.jsx
+++ frontend/src/components/BackButton.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import BackArrowSvg from '../assets/back-arrow-thin.svg?react';
 
 export default function BackButton({ onClick, children, color, className, style }) {
   return (
@@ -7,11 +8,7 @@ export default function BackButton({ onClick, children, color, className, style 
       className={`back-btn${className ? ' ' + className : ''}`}
       style={{ color: color || undefined, ...style }}
     >
-      <svg width="30" height="20" viewBox="0 0 36 20" fill="none"
-        stroke="currentColor" strokeWidth="3.5" strokeLinecap="round" strokeLinejoin="round">
-        <line x1="8" y1="10" x2="34" y2="10" />
-        <polyline points="16,2 8,10 16,18" />
-      </svg>
+      <BackArrowSvg width="24" height="24" style={{ fill: 'currentColor' }} />
       {children && <span style={{ fontSize: '18px' }}>{children}</span>}
     </button>
   );
